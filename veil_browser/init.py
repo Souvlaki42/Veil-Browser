89a93e27ba952c9ee85d0ecf10f3a62a06452bd3
Souvlaki42 <souvlaki420@gmail.com>
2025-11-07 22:49:59 +0200

    chore: removed old code and assets

--- veil_browser/init.py
+++
@@ -1,6 +0,0 @@
-# Package initialization file
-from .log_config import setup_logging
-from .browser_window import VeilBrowser
-from .title_bar import CustomTitleBar
-
-__all__ = ['setup_logging', 'VeilBrowser', 'CustomTitleBar']
